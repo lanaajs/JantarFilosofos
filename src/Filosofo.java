de9d5320cf434d416cb0b4d8759cb17af7277922
Alana de Jesus <lanaajesus93@gmail.com>
2024-10-14 20:34:33 -0300

    sem mensagens de pensando

--- src/Filosofo.java
+++ src/Filosofo.java
@@ -4,12 +4,17 @@ public class Filosofo extends Thread { // a classe Filosofo herda a funcionalida
     final static int TEMPO_MAXIMO = 1000; // 1 segundo
 
     // constante que define o tempo limite total que o filósofo vai executar o ciclo de pensar e comer
-    final static int TEMPO_LIMITE_EXECUCAO = 60000; // 1 minuto
+    final static int TEMPO_LIMITE_EXECUCAO = 300000; // 5 minutos
 
     Mesa mesa; // instância da classe Mesa gerencia o uso dos garfos compartilhados pelos filósofos
-    int idFilosofo; // identificador do filósofo (para diferenciar os filósofos)
-    private long totalPensando = 0; // armazenar o tempo total pensando
-    private long totalComendo = 0; // armazenar o tempo total comendo
+    int idFilosofo;
+
+    // armazenar o tempo total comendo e pensando
+    private long totalPensando = 0; 
+    private long totalComendo = 0; 
+
+    //controla se algum filósofo está comendo
+    private static boolean isComendo = false; 
 
     // construtor da classe Filosofo
     public Filosofo(String nome, Mesa mesaJantar, int numFil) {
@@ -19,54 +24,55 @@ public class Filosofo extends Thread { // a classe Filosofo herda a funcionalida
     }
 
     @Override // sobrescrevendo o método run() da classe Thread para definir o comportamento da thread
+
     // este método define o que o filósofo faz durante sua execução (alternando entre pensar e comer)
     public void run() {
-        long inicio = System.currentTimeMillis(); // armazena o momento em que a execução do filósofo começou
+        long inicio = System.currentTimeMillis(); //armazena o momento em que a execução do filósofo começou
 
-        // o loop continua enquanto o tempo total de execução não ultrapassar o
-        // TEMPO_LIMITE_EXECUCAO
+        // o loop continua enquanto o tempo total de execução não ultrapassar o TEMPO_LIMITE_EXECUCAO
         while (System.currentTimeMillis() - inicio < TEMPO_LIMITE_EXECUCAO) {
 
             // gera um tempo aleatório (sempre em milissegundos) para o filósofo pensar
             int tempo = (int) (Math.random() * TEMPO_MAXIMO);
-            totalPensando += tempo; // Acumula o tempo pensando
+            totalPensando += tempo; // acumula o tempo pensando
             pensar(tempo); // chama o método pensar com o tempo gerado
 
             // o filósofo tenta pegar os garfos para começar a comer
             mesa.pegarGarfos(idFilosofo);
 
-            // gera um tempo aleatório para o filósofo comer
-            tempo = (int) (Math.random() * TEMPO_MAXIMO);
-            totalComendo += tempo; // Acumula o tempo comendo
-            comer(tempo); // chama o método comer com o tempo gerado
+            //define que o filósofo está comendo, evitar imprimir as mensagens de pensando
+            synchronized (Filosofo.class) { //usando o Filosofo.class ccomo objeto de bloqueio
+                isComendo = true;
+            }
 
-            // o filósofo devolve os garfos após terminar de comer
+            tempo = (int) (Math.random() * TEMPO_MAXIMO);
+            totalComendo += tempo; 
+            comer(tempo); 
+
+            //o filósofo devolve os garfos após terminar de comer
             mesa.devolverGarfos(idFilosofo);
         }
 
-        // quando o tempo limite termina, o filósofo para de executar e exibe uma
-        // mensagem
-        System.out.println(getName() + " terminou de executar após atingir o tempo limite.");
+        System.out.println(getName() + " terminou de executar após atingir o tempo limite."); //quando o tempo limite termina
     }
 
     // método para simular o filósofo pensando por um tempo aleatório
     public void pensar(int tempo) {
-        System.out.println(getName() + " está pensando por " + tempo + "ms."); // mensagem informando que o filósofo
-                                                                               // está pensando
+        if (!isComendo) {
+            System.out.println(getName() + " está pensando por " + tempo + "ms.");
+        }
 
         try {
             Thread.sleep(tempo); // o filósofo "dorme" por um tempo aleatório, simulando o ato de pensar
         } catch (InterruptedException e) {
-            System.out.println(getName() + " foi interrompido enquanto pensava."); // eerro caso a thread seja
-                                                                                   // interrompida
+            System.out.println(getName() + " foi interrompido enquanto pensava."); // erro caso a thread seja interrompida
         }
     }
 
     // método para simular o filósofo comendo por um tempo aleatório
     public void comer(int tempo) {
-        System.out.println(getName() + " está comendo por " + tempo + "ms."); // mensagem informando que o filósofo está
-                                                                              // comendo
-
+        System.out.println(getName() + " está comendo por " + tempo + "ms."); // mensagem informando que o filósofo está comendo
+        
         try {
             Thread.sleep(tempo); // o filósofo "dorme" por um tempo aleatório, simulando o ato de comer
         } catch (InterruptedException e) {
@@ -74,7 +80,7 @@ public class Filosofo extends Thread { // a classe Filosofo herda a funcionalida
         }
     }
 
-    // métodos para obter os tempos
+    //métodos para obter os tempos
     public long getTempoPensando() {
         return totalPensando;
     }
@@ -83,16 +89,3 @@ public class Filosofo extends Thread { // a classe Filosofo herda a funcionalida
         return totalComendo;
     }
 }
-
-/*
- * Um bloco try é chamado de bloco protegido porque, caso ocorra algum problema
- * com os comandos dentro do bloco, a execução desviará para os blocos catch
- * correspondentes.
- * 
- * try >> permite que você defina um bloco de código a ser testado quanto a
- * erros enquanto ele está sendo executado.
- * catch >> permite que você defina um bloco de código a ser executado, caso
- * ocorra um erro no bloco try.
- * InterruptedException >> lançada quando uma thread é interrompida enquanto
- * está esperando (dormindo)
- */
--- src/JantarDosFilosofos.java
+++ src/JantarDosFilosofos.java
@@ -1,52 +1,57 @@
-/* quando o thread inicia, será linhas de execução separadas, no caso 5 linhas, referente aos 5 filosofos
-mas todos na mesma mesa, alternando entre comer e pensar */
+/*referencias: 
+https://deinfo.uepg.br/~alunoso/2021/SO/Filosofos_Java/
+https://blog.pantuza.com/artigos/o-jantar-dos-filosofos-problema-de-sincronizacao-em-sistemas-operacionais
+https://www.youtube.com/watch?v=9KAN4xsJo6Y
+*/
 
-import java.util.ArrayList; 
-import java.util.List; 
+import java.util.ArrayList;
+import java.util.List;
 
 public class JantarDosFilosofos {
     public static void main(String[] args) {
-        
-        int numFilosofos = 5; //número de filósofos
 
-        //cria uma instância da classe Mesa que gerencia os filosofos (garfos)
+        int numFilosofos = 5; // número de filósofos
+
+        // cria uma instância da classe Mesa que gerencia os filosofos (garfos)
         Mesa mesa = new Mesa(numFilosofos);
 
-        //lista para armazenar os filósofos
+        // lista para armazenar os filósofos
         List<Filosofo> filosofos = new ArrayList<>();
 
-        //criar os filósofos
+        // criar os filósofos
         for (int i = 0; i < numFilosofos; i++) {
 
-            //cria um novo filósofo, passando seu nome, a mesa e sua posição
+            // cria um novo filósofo, passando seu nome, a mesa e sua posição
             Filosofo filosofo = new Filosofo("Filósofo " + (i + 1), mesa, i);
 
-            filosofos.add(filosofo); //adiciona o filósofo à lista
-            filosofo.start(); //inicia a thread do filósofo
+            filosofos.add(filosofo); // adiciona o filósofo à lista
+            filosofo.start(); // inicia a thread do filósofo
         }
 
-        //aguardar todos os filósofos terminarem de comer e pensar
+        // aguardar todos os filósofos terminarem de comer e pensar
         for (Filosofo filosofo : filosofos) {
             try {
-                filosofo.join(); //aguarda ate que termine sua execução
+                filosofo.join(); // aguarda ate que termine sua execução
             } catch (InterruptedException e) {
-                System.out.println("A execução foi interrompida."); //caso ocorra uma interrupção na espera, exibe uma mensagem
+                System.out.println("A execução foi interrompida."); // caso ocorra uma interrupção na espera, exibe uma
+                                                                    // mensagem
             }
         }
 
         System.out.println("\nTEMPO TOTAL DOS FILOSOFOS:");
 
-        System.out.printf("%-15s %-20s %-20s %-20s %-20s%n", "NOME DO FILOSOFO", "TEMPO PENSANDO (ms)", "TEMPO PENSANDO (s)",
+        System.out.printf("%-15s %-20s %-20s %-20s %-20s%n", "NOME DO FILOSOFO", "TEMPO PENSANDO (ms)",
+                "TEMPO PENSANDO (s)",
                 "TEMPO COMENDO (ms)", "TEMPO COMENDO (s)");
 
-        //percorre a lista de filósofos para imprimir seus tempos
+        // percorre a lista de filósofos para imprimir seus tempos
         for (Filosofo filosofo : filosofos) {
-            
-            //tem armazenado o tempo que cada filósofo passou pensando e comendo
+
+            // tem armazenado o tempo que cada filósofo passou pensando e comendo
             long tempoPensandoMs = filosofo.getTempoPensando();
             long tempoComendoMs = filosofo.getTempoComendo();
 
-            //imprime os dados do filósofos em milissegundos e segundos
+            // imprime os dados do filósofos em milissegundos e segundos
             System.out.printf("%-15s %-20d %-20.2f %-20d %-20.2f%n",
                     filosofo.getName(),
                     tempoPensandoMs,
